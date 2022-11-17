8902b26d2448b447268f8e8749ff3b1ea3139de6
Raeme Khatib <raeme.khatib@telus.com>
2022-11-16 18:14:14 -0700

    created a count letter function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (allLetters) {
+  const results = {};
+  for (const letter of allLetters) {
+    if(letter === " ") continue; 
+    // i want to check results array
+    // add letters from result array
+    // get count of results array
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      // initializing the letter with 1
+      results[letter] = 1;
+    }
+  }
+  return results;
+}
+
+console.log(countLetters("lighthouse in the house"))
