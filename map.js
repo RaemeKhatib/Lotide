cad735511071aa9738c283fbf2fc633d6e06ff91
Raeme Khatib <raeme.khatib@telus.com>
2022-11-17 17:41:21 -0700

    added my assertequalsarray

--- map.js
+++ map.js
@@ -10,3 +10,23 @@ const map = function(array, callback){
 
 const results1 = map(words, word => word [0]);
 console.log(results1)
+
+
+const eqArrays = function (firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const assertArraysEqual = function(firstArray, secondArray) { 
+    if (eqArrays(firstArray, secondArray)) {
+      console.log( `✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+    } else {
+      console.log( `🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
