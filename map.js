dfd2b626957177cd0ec4906e297bf38e11eafebc
Raeme Khatib <raeme.khatib@telus.com>
2022-11-17 18:22:16 -0700

    was able to test assertions in map

--- map.js
+++ map.js
@@ -1,18 +1,18 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function(array, callback){
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    results.push(callback(item))
+    results.push(callback(item));
   }
   return results;
 };
 
 const results1 = map(words, word => word [0]);
-console.log(results1)
+console.log(results1);
 
 
-const eqArrays = function (firstArray, secondArray) {
+const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
       return false;
@@ -23,10 +23,16 @@ const eqArrays = function (firstArray, secondArray) {
 
 
 
-const assertArraysEqual = function(firstArray, secondArray) { 
-    if (eqArrays(firstArray, secondArray)) {
-      console.log( `✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
-    } else {
-      console.log( `🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
   }
 };
+// console.log(map(["ground", "second"], item => {
+//   if (item === "ground")
+//   return item;
+// }))
+
+assertArraysEqual(map(["ground", "second", "third"], item => item), ["ground", "second"]);
