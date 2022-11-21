46c9fadffe4e97b93071bfb1b36089671519af89
Raeme Khatib <raeme.khatib@telus.com>
2022-11-21 10:24:03 -0700

    refactored tail test

--- tail.js
+++ tail.js
@@ -12,7 +12,4 @@ const tail = function(item) {
 };
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length,2));
-console.log(assertEqual(result[0], "Lighthouse"));
-console.log(assertEqual(result[1], "Labs"));
+module.exports = tail;
